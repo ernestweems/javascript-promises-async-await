3e6d462516e22b1ddfca0dddf8dca2e55e5d9e40
unknown <ernestweems@gmail.com>
2020-10-13 08:04:24 -0400

    getting local JSON data back with simulated promise

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-export function fetchMovies() {}
+import { fetchWithTimeout } from './services';
+const movies = require('./data/movies.json');
+export function fetchMovies() {
+    const resloveFunction = () => movies;
+    return fetchWithTimeout(1000).then(resloveFunction);
+}
+
+const moviePromise = fetchMovies();
+moviePromise.then(results => {
+    console.log(results);
+})
