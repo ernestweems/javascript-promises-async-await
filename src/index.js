7d3b8ae29947a56377843676b1bb767857bb0aeb
unknown <ernestweems@gmail.com>
2020-10-13 08:20:08 -0400

    corrected mispelling

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import { fetchWithTimeout } from './services';
 const movies = require('./data/movies.json');
 export function fetchMovies() {
-    const resloveFunction = () => movies;
-    return fetchWithTimeout(1000).then(resloveFunction);
+    const resolveFunction = () => movies;
+    return fetchWithTimeout(1000).then(resolveFunction);
 }
 
 const moviePromise = fetchMovies();
